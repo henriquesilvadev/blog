0793bfddcb06eaf7024f979ead4f69327dfe1598
HENRIQUE SILVA DEV <contato@henriquesilva.dev>
2020-04-09 23:17:15 -0300

    Update Player

--- @lekoarts/gatsby-theme-minimal-blog/src/components/soundcloud-player.tsx
+++ @lekoarts/gatsby-theme-minimal-blog/src/components/soundcloud-player.tsx
@@ -9,11 +9,11 @@ class SoundCloudPlayer extends Component {
         <Title text={"Podcast"}/>
         <iframe 
           width="100%" 
-          height="300" 
+          height="300"
           scrolling="no" 
-          frameborder="no" 
+          frameBorder="no" 
           allow="autoplay" 
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/805156144&color=%23f2f2f2&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1029315298&color=%23f2f2f2&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
         </iframe>
       </div> 
     )
